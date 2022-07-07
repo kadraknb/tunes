438d86405d430c5e690a247821ade4f69f088b04
kadraknb <kadrak02@hotmail.com>
2022-07-07 00:31:56 -0300

    Requisitos 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from '../components/caregando';
+
+// const searchAlbumsAPI = require('../services/searchAlbumsAPI');
 
 class Search extends React.Component {
   constructor() {
@@ -8,6 +13,9 @@ class Search extends React.Component {
     this.state = {
       buttonOff: true,
       buscar: '',
+      nomeBusca: '',
+      artist: [],
+      carregar: false,
     };
   }
 
@@ -22,33 +30,68 @@ class Search extends React.Component {
     });
   }
 
-  submit = () => {
-    console.log('sim');
+  submit = async (aa) => {
+    const { buscar } = this.state;
+    aa.preventDefault();
+    this.setState({ carregar: true });
+    const artista = await searchAlbumsAPI(buscar);
+    // console.log(artista);
+    this.setState({
+      artist: artista,
+      nomeBusca: buscar,
+      buscar: '',
+      carregar: false,
+    });
   }
 
   render() {
-    const { buscar, buttonOff } = this.state;
+    const { buscar, buttonOff, artist, nomeBusca, carregar } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
         <form>
-          <span>
-            <input
-              type="text"
-              onChange={ (aa) => this.change(aa) }
-              value={ buscar }
-              data-testid="search-artist-input"
-            />
-            <button
-              type="submit"
-              onClick={ this.submit }
-              disabled={ buttonOff }
-              data-testid="search-artist-button"
-            >
-              Pesquisar
-            </button>
-          </span>
+          {carregar ? (
+            <Carregando />
+          ) : (
+            <span>
+              <input
+                type="text"
+                onChange={ (aa) => this.change(aa) }
+                value={ buscar }
+                data-testid="search-artist-input"
+              />
+              <button
+                type="submit"
+                onClick={ (aa) => this.submit(aa) }
+                disabled={ buttonOff }
+                data-testid="search-artist-button"
+              >
+                Pesquisar
+              </button>
+            </span>
+          )}
         </form>
+        {artist[0] ? (
+          <>
+            <span>
+              {`Resultado de álbuns de: ${nomeBusca}`}
+            </span>
+            <ul>
+              {artist.map((aa) => (
+                <li key={ aa.collectionId }>
+                  <Link
+                    data-testid={ `link-to-album-${aa.collectionId}` }
+                    to={ `/album/${aa.collectionId}` }
+                  >
+                    {aa.collectionName}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          nomeBusca && <p>Nenhum álbum foi encontrado</p>
+        )}
         Search
       </div>
     );
