26fc61e601b5384bb56f09dc5be26889d87a15ab
kadranb <kadrak02@hotmail.com>
2022-12-14 14:14:46 -0300

    add build

--- src/App.js
+++ src/App.js
@@ -7,35 +7,36 @@ import Favorites from './pages/favorites';
 import Profile from './pages/profile';
 import ProfileEdit from './pages/ProfileEdit';
 import NotFound from './pages/NotFound';
-import './tuner.css'
-
+import './tuner.css';
 
 class App extends React.Component {
-
   render() {
     return (
-      <div id='tunerBody'>
-      <div id='mein_box'>
-      <BrowserRouter>
-        <Switch>
-        <Route exact path="/tunes/" component={ Login } />
-          <Route path="/tunes/search" component={ Search } />
-          <Route path="/tunes/album/:id" render={ (props) => <Album { ...props } /> } />
-          <Route path="/tunes/favorites" component={ Favorites } />
-          <Route path="/tunes/profile/edit" component={ ProfileEdit } />
-          <Route path="/tunes/profile" component={ Profile } />
-          <Route path="/tunes/*" component={ NotFound } />
+      <div id="tunerBody">
+        <div id="mein_box">
+          <BrowserRouter>
+            <Switch>
+              <Route path="/tunes/" component={Login} />
+              <Route path="/tunes/search" component={Search} />
+              <Route
+                path="/tunes/album/:id"
+                render={(props) => <Album {...props} />}
+              />
+              <Route path="/tunes/favorites" component={Favorites} />
+              <Route path="/tunes/profile/edit" component={ProfileEdit} />
+              <Route path="/tunes/profile" component={Profile} />
+              <Route path="/tunes/*" component={NotFound} />
 
-          <Route exact path="/" component={ Login } />
+              {/* <Route exact path="/" component={ Login } />
           <Route path="/search" component={ Search } />
           <Route path="/album/:id" render={ (props) => <Album { ...props } /> } />
           <Route path="/favorites" component={ Favorites } />
           <Route path="/profile/edit" component={ ProfileEdit } />
           <Route path="/profile" component={ Profile } />
-          <Route path="/*" component={ NotFound } />
-        </Switch>
-      </BrowserRouter>
-      </div>
+          <Route path="/*" component={ NotFound } /> */}
+            </Switch>
+          </BrowserRouter>
+        </div>
       </div>
     );
   }
